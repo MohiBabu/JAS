0debb5361eaacb1616c133003e5bdec8a895d675
MohiBabu <baburngl@gmail.com>
2024-05-18 13:13:09 +0530

    Initial Auth Module Files Added

---
+++ frontend/src/modules/Auth/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ForgotPassword extends Component {
+  render() {
+	return (
+	  <div>
+		ForgotPassword
+	  </div>
+	)
+  }
+}
+
+export default ForgotPassword
---
+++ frontend/src/modules/Auth/UpdatePassword.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UpdatePassword extends Component {
+  render() {
+	return (
+	  <div>
+		UpdatePassword
+	  </div>
+	)
+  }
+}
+
+export default UpdatePassword
